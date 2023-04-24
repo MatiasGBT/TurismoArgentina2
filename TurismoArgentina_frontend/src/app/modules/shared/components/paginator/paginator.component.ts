fb131d1dae1f19927966b5fe1054438f22c0027a
Matías Gabriel Blanco Toledo <blancotoledomatiasgabriel@gmail.com>
2023-04-24 11:46:50 -0300

    Improved paginator
    - Added the number of pages in view
    - Added the possibility to go to a desired page by typing its number

--- TurismoArgentina_frontend/src/app/modules/shared/components/paginator/paginator.component.ts
+++ TurismoArgentina_frontend/src/app/modules/shared/components/paginator/paginator.component.ts
@@ -1,21 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.css']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent {
   @Input() page: number = 0;
   @Input() isFirstPage: boolean = true;
   @Input() isLastPage: boolean = false;
   @Input() totalPages: number = 1;
   @Output() changePageEvent = new EventEmitter<number>();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @ViewChild("inputPage") inputPage: ElementRef | undefined;
 
   public goFirstPage(): void {
     this.changePageEvent.emit(0);
@@ -31,5 +27,15 @@ export class PaginatorComponent implements OnInit {
 
   public goLastPage(): void {
     this.changePageEvent.emit(this.totalPages - 1);
+  }
+
+  public goToSelectedPage(event: any): void {
+    if ((event.key === 'Enter' || event.keyCode == 13) && this.inputPage) {
+      let pageToGo = this.inputPage.nativeElement.value -1;
+      if (pageToGo < 0) pageToGo = 0;
+      if (pageToGo > this.totalPages - 1) pageToGo = this.totalPages - 1;
+      this.inputPage.nativeElement.value = pageToGo + 1;
+      this.changePageEvent.emit(pageToGo);
+    }
   }
 }
